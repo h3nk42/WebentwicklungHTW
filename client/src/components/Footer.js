2727a9c4687bdf363eb030bebd88b797efd44719
sunanmau5 <s0566144@htw-berlin.de>
2021-01-05 14:54:01 +0100

    added register, login and darkmode

---
+++ client/src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavLink, useHistory} from 'react-router-dom';
+import {useTranslation} from "react-i18next";
+
+function Footer() {
+
+    const {t, i18n} = useTranslation();
+
+    return (
+        <nav className="navbar navbar-light footer mb-3">
+            <div className="row col-12 d-flex align-items-center text-white">
+                <NavLink className="px-3 text-white"
+                         exact
+                         to="/impressum"
+                >
+                    {t("impressum_footer")}
+                </NavLink>
+                <NavLink className="px-3 text-white"
+                         exact
+                         to="/datenschutz"
+                >
+                    {t("datenschutz_footer")}
+                </NavLink>
+            </div>
+        </nav>
+    );
+}
+
+export default Footer;
