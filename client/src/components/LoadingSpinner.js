082c6313dd6c44aa6b56d4a589561381d81e088f
dropqcom <fxscholler@protonmail.com>
2021-01-18 13:46:48 +0100

    add-Plan-page and minor Header changes + stlyes

---
+++ client/src/components/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+// styleSpinner (Font Awesome classes)
+
+const LoadingSpinner = () => (
+    <div>
+        <i className="fa fa-spinner fa-spin" /> Loading...
+    </div>
+);
+
+export default LoadingSpinner;
+
+
