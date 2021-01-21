2c8efb58c10c6341639fd4660a070806f55ca0cd
Henk <henk.vandrsloot@gmail.com>
2021-01-21 10:23:04 +0100

    remerge removeUserFromPlan into main

--- backend/models/Plan.js
+++ backend/models/Plan.js
@@ -1,53 +1,66 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const {retErr} = require('../utils/index');
+const { retErr } = require("../utils/index");
 
 const PlanSchema = new Schema(
-    {
-        users: [],
-        tasks: [],
-        owner: {
-            type: String,
-            required: true
-        },
-        name: {
-            type: String,
-            required: true
-        },
+  {
+    users: [],
+    tasks: [],
+    owner: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 PlanSchema.methods.delete = function (res) {
-    const plan = this;
-    return new Promise((resolve, reject) => {
-        plan.deleteOne({_id: this._id}, (err, data) => {
-            if (err) reject(retErr(res, {}, 418, 'DB_ERROR'));
-            resolve({success: true, data: data});
-        })
-    })
-}
+  const plan = this;
+  return new Promise((resolve, reject) => {
+    plan.deleteOne({ _id: this._id }, (err, data) => {
+      if (err) reject(retErr(res, {}, 418, "DB_ERROR"));
+      resolve({ success: true, data: data });
+    });
+  });
+};
 
 PlanSchema.methods.create = function (res) {
-    const plan = this;
-    return new Promise((resolve, reject) => {
-        plan.save({_id: this._id}, (err, plan) => {
-            if (err) reject(retErr(res, {}, 418, 'DB_ERROR'));
-            resolve({success: true, data: plan});
-        })
-    })
-}
+  const plan = this;
+  return new Promise((resolve, reject) => {
+    plan.save({ _id: this._id }, (err, plan) => {
+      if (err) reject(retErr(res, {}, 418, "DB_ERROR"));
+      resolve({ success: true, data: plan });
+    });
+  });
+};
 
 PlanSchema.methods.addUser = function (res, userToAdd) {
-    return new Promise((resolve, reject) => {
-        this.users.push({userName: userToAdd, points: 0})
-        this.save((err, plan) => {
-            if (err) reject(retErr(res, {}, 418, 'DB_ERROR'));
-            else { resolve({success: true, plan: plan});
-            }
-        })
-    })
-}
-
+  return new Promise((resolve, reject) => {
+    this.users.push({ userName: userToAdd, points: 0 });
+    this.save((err, plan) => {
+      if (err) reject(retErr(res, {}, 418, "DB_ERROR"));
+      else {
+        resolve({ success: true, plan: plan });
+      }
+    });
+  });
+};
+PlanSchema.methods.removeUser = function (res, userToRemove) {
+  return new Promise((resolve, reject) => {
+    this.users = this.users.filter((user, index, arr) => {
+      return user.userName !== userToRemove;
+    });
+    this.save((err, plan) => {
+      if (err) reject(retErr(res, {}, 418, "DB_ERROR"));
+      else {
+        resolve({ success: true, plan: plan });
+      }
+    });
+  });
+};
 // export the new Schema so we could modify it using Node.js
 module.exports = mongoose.model("Plan", PlanSchema, "Plan");
--- backend/routes/plan.js
+++ backend/routes/plan.js
@@ -1,40 +1,43 @@
-const express = require('express');
-const planControl = require('../controllers/plans')
-const passport = require('passport')
-const {body} = require('express-validator');
-
+const express = require("express");
+const planControl = require("../controllers/plans");
+const passport = require("passport");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
-
 router.get(
-    '/showOne',
-    passport.authenticate('jwt',{session: false}),
-    planControl.showOne);
+  "/showOne",
+  passport.authenticate("jwt", { session: false }),
+  planControl.showOne
+);
 
-router.get(
-    '/showMany',
-    planControl.showMany);
+router.get("/showMany", planControl.showMany);
 
 router.post(
-    '/create',
-    [
-        body('name').not().isEmpty(),
-            body('name').isLength({min: 1, max: 15})],
-    passport.authenticate('jwt',{session: false}),
-    planControl.create);
+  "/create",
+  [body("name").not().isEmpty(), body("name").isLength({ min: 1, max: 15 })],
+  passport.authenticate("jwt", { session: false }),
+  planControl.create
+);
 
 router.delete(
-    '/delete',
-    passport.authenticate('jwt',{session: false}),
-    planControl.destroy);
+  "/delete",
+  passport.authenticate("jwt", { session: false }),
+  planControl.destroy
+);
 
 router.post(
-    '/addUser',
-    [
-        body('userName').not().isEmpty().withMessage(' username is required')],
-    passport.authenticate('jwt',{session: false}),
-    planControl.addUser )
+  "/addUser",
+  [body("userName").not().isEmpty().withMessage(" username is required")],
+  passport.authenticate("jwt", { session: false }),
+  planControl.addUser
+);
 
+router.post(
+  "/removeUser",
+  [body("userName").not().isEmpty().withMessage(" username is required")],
+  passport.authenticate("jwt", { session: false }),
+  planControl.removeUser
+);
 
 module.exports = router;
